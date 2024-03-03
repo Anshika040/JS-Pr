ff5787214f875c272e5fea06bb780c0fca4e2b68
Anshika040 <140148419+Anshika040@users.noreply.github.com>
2024-03-03 17:57:55 +0000

    type conversion

---
+++ 01_basics/type_conversions.js
@@ -0,0 +1,13 @@
+let isloggedin="hitesh"
+
+let booleanisloggedin = Boolean(isloggedin);
+
+console.log(booleanisloggedin);
+
+//1 =>true; 0 =>false
+// "anshika"=>true
+
+let n=33
+let sn=String(n)
+console.log(sn);
+console.log(typeof sn);
